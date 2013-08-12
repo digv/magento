7c3bd04ec7f415ff2e3db74bc34641e94e2c7b33
gefferey <250276522@qq.com>
2013-08-12 23:03:06 +0800

    zjh

---
+++ app/code/local/Lightbox/Product/Block/Product/Abstract.php
@@ -0,0 +1,6 @@
+<?php
+
+class Lightbox_Product_Block_Product_Abstract extends Mage_Catalog_Block_Product_Abstract {
+	
+	
+}
---
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -0,0 +1,8 @@
+<?php
+
+class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Product_Abstract {
+	
+	public function test () {
+		echo 'zjh';
+	}
+}
