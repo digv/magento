6634ea587663ea5c3414c6d10b99eb85a51e1a19
gefferey <250276522@qq.com>
2013-08-13 22:41:37 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -3,17 +3,7 @@
 class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Product_Abstract {
     protected function _construct()
     {
-        parent::_construct();
-
-        /*
-         * In case template was passed through constructor
-         * we assign it to block's property _template
-         * Mainly for those cases when block created
-         * not via Mage_Core_Model_Layout::addBlock()
-         */
-        if ($this->hasData('template')) {
-            $this->setTemplate($this->getData('template'));
-        }
+        $this->setTemplate('product/latest/latest.phtml');
     }
 	public function test () {
 		echo 'zjh';
