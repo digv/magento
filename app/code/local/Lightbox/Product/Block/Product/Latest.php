e1e9976b9fcf9d707a17587e6da2cdd225cd6eec
gefferey <250276522@qq.com>
2013-08-13 22:44:04 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -1,10 +1,6 @@
 <?php
 
 class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Product_Abstract {
-    protected function _construct()
-    {
-        $this->setTemplate('product/latest/latest.phtml');
-    }
 	public function test () {
 		echo 'zjh';
 	}
