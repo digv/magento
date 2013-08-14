27e170ffa5b1198602b7e091c30241d0e2694227
gefferey <250276522@qq.com>
2013-08-14 21:30:57 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -1,7 +1,19 @@
 <?php
 
 class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Product_Abstract {
-	public function test () {
-		echo 'zjh';
-	}
+	protected $_title = 'Latest';
+    protected $_priceSuffix = '-latest';
+    protected $_className = 'bmproducts-latest';
+    
+    protected function _beforeToHtml()
+    {
+        $collection = $this->getCollection('bmproducts/reports_product_collection')
+
+            ->addAttributeToSelect('updated_at')
+            ->setOrder('updated_at', 'desc');
+        
+        $this->setProductCollection($collection);
+
+        return parent::_beforeToHtml();
+    } 
 }
