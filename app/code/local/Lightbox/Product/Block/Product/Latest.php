ff2cff9a9e07c6e54478faa5c3b8b8d1e68b7570
gefferey <250276522@qq.com>
2013-08-17 21:02:04 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -6,7 +6,7 @@ class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Produ
     protected $_className = 'bmproducts-latest';
     
     protected function _beforeToHtml() {
-		$collection = $this->getCollection ( 'catalog/reports_product_collection' )->
+		$collection = $this->getCollection ( 'reports/product_collection' )->
 
 		addAttributeToSelect ( 'updated_at' )->setOrder ( 'updated_at', 'desc' );
 		
