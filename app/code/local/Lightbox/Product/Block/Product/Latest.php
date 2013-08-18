45f475a4b270a87a4cfacc071a6cc5555908be2f
gefferey <250276522@qq.com>
2013-08-18 18:26:17 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -4,7 +4,7 @@ class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Produ
 	protected $_title = 'Latest';
     protected $_priceSuffix = '-latest';
     protected $_className = 'bmproducts-latest';
-    
+    protected $_defaultColumnCount = 6;
     protected function _beforeToHtml() {
 		$collection = $this->getCollection ( 'reports/product_collection' )
 						-> addAttributeToSelect ( 'updated_at' )
