bfcd042b51ee3bb3a3c6eb4ab55ca4a470c91ed2
gefferey <250276522@qq.com>
2013-08-19 20:32:42 +0800

    d

--- app/code/local/Lightbox/Product/Block/Product/Latest.php
+++ app/code/local/Lightbox/Product/Block/Product/Latest.php
@@ -12,7 +12,7 @@ class Lightbox_Product_Block_Product_Latest extends Lightbox_Product_Block_Produ
 						-> setPageSize('10')
 						-> setCurPage('1');
 		
-						//var_dump((string) $collection -> getSelect());
+						var_dump($collection -> getSelect()->__toString());
 		$this->setProductCollection ( $collection );
 		
 		return parent::_beforeToHtml ();
